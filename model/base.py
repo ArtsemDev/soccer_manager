fdc5d78d58d13ba9319c719e61bcd17a2a7b86b1
dev <thommy.python.dev@gmail.com>
2023-03-08 14:05:35 +0300

    initial commit

--- model/base.py
+++ model/base.py
@@ -1,7 +1,9 @@
 from typing import Type, Any, Sequence
+from inspect import iscoroutine
 
 from sqlalchemy import Column, INT, create_engine, Row, RowMapping, select
 from sqlalchemy.orm import DeclarativeBase, declared_attr, sessionmaker, Session
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 
 class Base(DeclarativeBase):
@@ -10,17 +12,24 @@ class Base(DeclarativeBase):
     engine = create_engine('sqlite:///db.sqlite3')
     session = sessionmaker(bind=engine)
 
+    async_engine = create_async_engine('sqlite+aiosqlite:///db.sqlite3')
+    async_session = async_sessionmaker(bind=async_engine)
+
     @declared_attr
     def __tablename__(cls):
         return ''.join(f'_{i.lower()}' if i.isupper() else i for i in cls.__name__).strip('_')
 
     @staticmethod
     def create_session(func):
+        async def async_wrapper(*args, **kwargs):
+            async with Base.async_session() as session:
+                return await func(*args, **kwargs, session=session)
+
         def wrapper(*args, **kwargs):
             with Base.session() as session:
                 return func(*args, **kwargs, session=session)
 
-        return wrapper
+        return async_wrapper if iscoroutine(func) else wrapper
 
     @create_session
     def save(self, session: Session = None) -> None:
